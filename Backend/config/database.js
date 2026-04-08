606d50853d49c7a9fbbcfdf4855784ee1483cef7
Nitish2909 <nitishraj7043@gmail.com>
2026-04-08 20:18:10 +0530

    assignment

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -3,19 +3,33 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD ,
-    database: process.env.DB_NAME
-})
+
+// Replace with your actual credentials
+
+
+const db = mysql.createConnection(process.env.connectionString);
 
 db.connect((err) => {
-    if (err) {
-        console.error("Error connecting to the database:", err)
-        return
-    }
-    console.log("Connected to the database")
-})
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+    return;
+  }
+  console.log('Successfully connected to the MySQL database.');
+});
+
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD ,
+//     database: process.env.DB_NAME
+// })
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error("Error connecting to the database:", err)
+//         return
+//     }
+//     console.log("Connected to the database")
+// })
 
 export default db;
