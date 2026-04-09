0dd68c992d376b19711ffab26cb7ae653b81f50b
Nitish2909 <nitishraj7043@gmail.com>
2026-04-09 19:28:40 +0530

    complete

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -13,7 +13,6 @@ db.connect((err) => {
   console.log('Successfully connected to the MySQL database.');
 });
 
-/* for local */
 
 // const db = mysql.createConnection({
 //     host: process.env.DB_HOST,
